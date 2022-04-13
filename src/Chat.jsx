03bc1287e983dde69abcab71d62ebe7bb150a318
tanle905 <tanle905@gmail.com>
2022-04-13 21:23:54 +0700

    final of final

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -108,19 +108,21 @@ const ChatMessage = React.memo(
               </audio>
             </div>
           )}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5 cursor-pointer self-center text-gray-800 transition hover:-translate-y-0.5 dark:text-gray-400"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            onClick={() => handleDeleteMessage(message)}
-          >
-            <path
-              fillRule="evenodd"
-              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-              clipRule="evenodd"
-            />
-          </svg>
+          {messageClass === "sent" && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 cursor-pointer self-center text-gray-800 transition hover:-translate-y-0.5 dark:text-gray-400"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              onClick={() => handleDeleteMessage(message)}
+            >
+              <path
+                fillRule="evenodd"
+                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                clipRule="evenodd"
+              />
+            </svg>
+          )}
           <p className="self-center text-xs font-thin text-gray-800 dark:text-gray-400 sm:text-sm">
             {date}
           </p>
