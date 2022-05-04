66d0baa4c0d679d073e12191ae485887bc6277bf
tanle905 <tanle905@gmail.com>
2022-05-04 11:33:16 +0700

    fianl

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -36,6 +36,7 @@ export default function Admin({ username, password }) {
       video,
       audio,
       file,
+      deleted,
       fileName,
       text,
       photoURL,
@@ -44,16 +45,17 @@ export default function Admin({ username, password }) {
     const date =
       createdAt != null &&
       moment(createdAt.toDate()).locale("vi").format("lll");
-    {
-      return (
-        <div className={`flex space-x-2 space-y-2`}>
+      return !deleted ? (
+        <div className={`flex space-x-2 space-y-2 group`}>
           <img
-            className={`mb-1 mt-auto h-6 w-6 rounded-full ring-2 ring-blue-500 dark:ring-indigo-600`}
+            className={`mb-1 mt-auto h-4 w-4 rounded-full ring-2 ring-blue-500 dark:ring-indigo-60`}
             src={photoURL}
             alt=""
           />
           {text ? (
-            <p className="max-w-[15rem] rounded-xl bg-blue-500 p-2 py-1 dark:bg-indigo-500 sm:max-w-xl">
+            <p
+              className={`max-w-[15rem] rounded-md p-2 py-1 sm:max-w-xl color`}
+            >
               {text}
             </p>
           ) : (
@@ -75,7 +77,9 @@ export default function Admin({ username, password }) {
             />
           )}
           {file && (
-            <div className="flex w-60 rounded-md bg-blue-500 p-2 text-gray-100 dark:bg-indigo-600">
+            <div
+              className={`flex w-60 rounded-md p-2 text-gray-100 color`}
+            >
               <p className="mr-3 truncate text-center text-sm">{fileName}</p>
               <div
                 className="cursor-pointer rounded-md bg-gray-200 p-1 transition hover:-translate-y-0.5 hover:bg-gray-300"
@@ -99,32 +103,46 @@ export default function Admin({ username, password }) {
             </div>
           )}
           {audio && (
-            <div className="w-64 rounded-md bg-blue-500 p-2 pb-0 text-gray-100 dark:bg-indigo-600">
+            <div
+              className={`w-64 rounded-md p-2 pb-0 text-gray-100 color`}
+            >
               <p className="truncate text-sm">{fileName}</p>
               <audio controls className="w-full py-2">
                 <source src={audio} />
               </audio>
             </div>
           )}
+          {(
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 cursor-pointer self-center text-gray-800 transition hover:text-gray-500 lg:opacity-0 lg:group-hover:opacity-100 dark:text-gray-400"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              onClick={() => handleDeleteMessage(message)}
+            >
+              <path
+                fillRule="evenodd"
+                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                clipRule="evenodd"
+              />
+            </svg>
+          )}
           <p className="self-center text-xs font-thin text-gray-800 dark:text-gray-400 sm:text-sm">
             {date}
           </p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="self-center h-5 w-5 cursor-pointer text-gray-800 transition hover:-translate-y-0.5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            onClick={() => handleDeleteMessage(message)}
-          >
-            <path
-              fillRule="evenodd"
-              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-              clipRule="evenodd"
-            />
-          </svg>
+        </div>
+      ) : (
+        <div className={`flex space-x-2 space-y-2 `}>
+          <img
+            className={`mb-1 mt-auto h-4 w-4 rounded-full ring-2 ring-blue-500 dark:ring-indigo-600`}
+            src={photoURL}
+            alt=""
+          />
+          <div className="flex w-60 rounded-md border-2 border-gray-300 dark:border-gray-700 bg-transparent p-2 text-gray-400">
+            <p>Message has been deleted.</p>
+          </div>
         </div>
       );
-    }
   });
 
   return (
